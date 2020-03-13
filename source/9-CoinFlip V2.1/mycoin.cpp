56f9bfcaa179a39c18e2b0968f1459e427b93cd5
Kearney <59185302+BackMountainDevil@users.noreply.github.com>
2020-03-13 21:13:59 +0800

    V2.1

---
+++ source/9-CoinFlip V2.1/mycoin.cpp
@@ -0,0 +1,82 @@
+﻿#include "mycoin.h"
+#include<QDebug>
+#include<QTimer>
+MyCoin::MyCoin(QString butImg)
+{
+    QPixmap pixmap;
+        bool ret = pixmap.load(butImg);
+        if(!ret)
+        {
+            qDebug() << butImg << "加载图片失败!";
+        }
+
+        this->setFixedSize( pixmap.width(), pixmap.height() );
+        this->setStyleSheet("QPushButton{border:0px;}");
+        this->setIcon(pixmap);
+        this->setIconSize(QSize(pixmap.width(),pixmap.height()));
+
+        timer1 = new QTimer(this);
+        timer2 = new QTimer(this);
+
+        isWin = false;
+        //监听正面翻转的信号槽
+            connect(timer1,&QTimer::timeout,[=](){
+                QPixmap pixmap;
+                QString str = QString(":/res/Coin000%1.png").arg(this->min++);
+                pixmap.load(str);
+                this->setFixedSize(pixmap.width(),pixmap.height() );
+                this->setStyleSheet("QPushButton{border:0px;}");
+                this->setIcon(pixmap);
+                this->setIconSize(QSize(pixmap.width(),pixmap.height()));
+                if(this->min > this->max) //如果大于最大值，重置最小值，并停止定时器
+                {
+                    this->min = 1;
+                     isAnimation = false;
+                    timer1->stop();
+                }
+            });
+
+            connect(timer2,&QTimer::timeout,[=](){
+                    QPixmap pixmap;
+                    QString str = QString(":/res/Coin000%1.png").arg((this->max)-- );
+                    pixmap.load(str);
+                    this->setFixedSize(pixmap.width(),pixmap.height() );
+                    this->setStyleSheet("QPushButton{border:0px;}");
+                    this->setIcon(pixmap);
+                    this->setIconSize(QSize(pixmap.width(),pixmap.height()));
+                    if(this->max < this->min) //如果小于最小值，重置最大值，并停止定时器
+                    {
+                        this->max = 8;
+                        isAnimation = false;
+                        timer2->stop();
+                    }
+                });
+
+}
+
+void MyCoin::changeFlag()
+{
+    if(this->flag) //如果是正面，执行下列代码
+    {
+               timer1->start(30);
+        this->flag = false;
+    }
+    else //反面执行下列代码
+    {
+                timer2->start(30);
+        this->flag = true;
+    }
+     isAnimation = true;
+}
+
+void MyCoin::mousePressEvent(QMouseEvent *e)
+{
+    if(this->isAnimation || isWin)
+    {
+        return;
+    }
+    else
+    {
+        return QPushButton::mousePressEvent(e);
+    }
+}
---
+++ source/9-CoinFlip V2.1/playscene.h
@@ -0,0 +1,32 @@
+﻿#ifndef PLAYSCENE_H
+#define PLAYSCENE_H
+
+#include <QMainWindow>
+#include "mycoin.h"
+
+
+class PlayScene : public QMainWindow
+{
+    Q_OBJECT
+public:
+    PlayScene(int levelNum);
+    //重写paintEvent事件
+    void paintEvent(QPaintEvent *);
+    //重置金币正反面到原始状态
+    void recoin();
+    void checkWin();
+
+
+    int levelIndex ; //内部成员属性 记录所选的关卡
+    bool isWin = true; //是否胜利
+    int gameArray[4][4]; //二维数组数据
+    MyCoin * coinBtn[4][4];
+    PlayScene * play = NULL;
+
+
+signals:
+    void chooseSceneBack();
+
+};
+
+#endif // PLAYSCENE_H
